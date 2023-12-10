4dbcc8755da4475e898308f9ba8a6a93528107ce
dynamicrevs <153166663+dynamicrevs@users.noreply.github.com>
2023-12-10 21:31:57 +0530

    Create carreverseparking.ino

---
+++ examples/carreverseparking/carreverseparking.ino
@@ -0,0 +1,41 @@
+Int trigpin=2;
+Int echopin=3;
+long duration;
+int distance;
+int BUZZER=10;
+voidsetup()
+{
+pinMode(trigpin,OUTPUT);
+pinMode(echopin,INPUT);
+pinMode(BUZZER,OUTPUT);
+Serial.begin(9600);
+}
+voidloop()
+{
+digitalWrite(trigpin,LOW);
+delayMicroseconds(3);
+digitalWrite(trigpin,HIGH);
+delayMicroseconds(11);
+digitalWrite(trigpin,LOW);
+duration=pulseIn(echopin,HIGH);
+Serial.println(echopin);
+Serial.println(duration);
+distance=duration*0.034/2;
+Serial.print("DISTANCE IN CM");
+Serial.println(distance); if((distance<=10))
+digitalWrite(BUZZER,HIGH);
+elseif((distance>10)&&(distance<=50))
+{
+digitalWrite(BUZZER,HIGH);
+delay(100);
+digitalWrite(BUZZER,LOW);
+delay(100);
+}
+elseif((distance>50)&&(distance<=200))
+{ digitalWrite(BUZZER,HIGH);
+delay(500);
+digitalWrite(BUZZER,LOW);
+delay(500);
+}
+else digitalWrite(BUZZER,LOW);
+}
